5b1abc13e3b762d4138e5d613651ab9ab0021272
Mehmet <mathdenizi@gmail.com>
2024-04-21 05:06:43 +0200

    sending request & Responses

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const fs = require("fs");
+const http = require("http");
 
-const userName = "math";
+const server = http.createServer((req, res) => {
+  console.log(req.method, req.url);
 
-fs.writeFile("user-data.txt", "Name " + userName, (err) => {
-  if (err) {
-    console.log(err);
+  if (req.method === "POST") {
+    console.log("post data");
+    let body = "";
+    req.on("end", () => {
+      const userName = body.split("=")[1];
+      res.end("<h1>" + userName + "</h1>");
+    });
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      '<form method="POST"><input type="text" name="username"> </input> <button type="submit"> Create user </button> </form>'
+    );
+    console.log("take data from browser");
   }
-  console.log("WROTE FILE");
 });
 
-fs.writeFile("user-data.txt", "Name " + userName, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("WROTE FILE");
-});
+server.listen(5001);
