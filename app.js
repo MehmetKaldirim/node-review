2b85ad4c1be8da3f861afdbe8050313368298cc3
Mehmet <mathdenizi@gmail.com>
2024-04-21 06:31:03 +0200

    expres send requests and responses

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  console.log(req.method, req.url);
+const app = express();
 
-  if (req.method === "POST") {
-    console.log("post data");
-    let body = "";
-    req.on("end", () => {
-      const userName = body.split("=")[1];
-      res.end("<h1>" + userName + "</h1>");
-    });
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      '<form method="POST"><input type="text" name="username"> </input> <button type="submit"> Create user </button> </form>'
-    );
-    console.log("take data from browser");
-  }
+app.use((req, res, next) => {
+  let body = "";
+  console.log(1);
+  req.on("end", () => {
+    console.log(2);
+    const userName = body.split("=")[1];
+    if (userName) {
+      console.log(4);
+      req.body = { name: userName };
+    }
+
+    next();
+    //res.end("<h1>" + userName + "</h1>");
+  });
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
 });
 
-server.listen(5001);
+app.use((req, res, next) => {
+  if (req.body) {
+    console.log(5);
+    return res.send("<h1>" + req.body.name + "</h1>");
+  }
+  console.log(3);
+  res.send(
+    '<form method="POST"><input type="text" name="username"> </input> <button type="submit"> Create user </button> </form>'
+  );
+});
+app.listen(5001);
