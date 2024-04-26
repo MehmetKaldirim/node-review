725923d32ecd759d95b039cb10203e63e1950215
Mehmet <mathdenizi@gmail.com>
2024-04-26 20:01:56 +0200

    usage of env variable

--- mongoose.js
+++ mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const Product = require("./models/product");
+require("dotenv").config();
 
-const MONGO_URI = `mongodb+srv://math:${process.env.MONGO_PASSWORD}@cluster0.89wbtaz.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`;
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.89wbtaz.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose
   .connect(MONGO_URI)
